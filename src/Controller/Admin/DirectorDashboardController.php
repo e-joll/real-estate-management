120b33b65a4e865b1fdc5462ee83ba0116811630
e-joll <eleonore.jollivet@gmail.com>
2024-09-29 21:11:04 +0200

    Rename file + add menu items

--- src/Controller/Admin/DirectorDashboardController.php
+++ src/Controller/Admin/DirectorDashboardController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controller\Admin;
 
+use App\Controller\Admin\Director\AppointmentCrudController;
+use App\Controller\Admin\Director\FeatureCrudController;
+use App\Controller\Admin\Director\InquiryCrudController;
+use App\Controller\Admin\Director\NotificationCrudController;
+use App\Controller\Admin\Director\PropertyCrudController;
+use App\Controller\Admin\Director\UserCrudController;
+use App\Entity\Appointment;
+use App\Entity\Feature;
+use App\Entity\Inquiry;
+use App\Entity\Notification;
+use App\Entity\Property;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminDashboardController extends AbstractDashboardController
+class DirectorDashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
@@ -41,6 +53,17 @@ class AdminDashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class)
+            ->setController(UserCrudController::class);
+        yield MenuItem::linkToCrud('Caractéristiques', 'fas fa-cogs', Feature::class)
+            ->setController(FeatureCrudController::class);
+        yield MenuItem::linkToCrud('Propriétés', 'fas fa-home', Property::class)
+            ->setController(PropertyCrudController::class);
+        yield MenuItem::linkToCrud('Demandes', 'fas fa-question-circle', Inquiry::class)
+            ->setController(InquiryCrudController::class);
+        yield MenuItem::linkToCrud('Rendez-vous', 'fas fa-calendar', Appointment::class)
+            ->setController(AppointmentCrudController::class);
+        yield MenuItem::linkToCrud('Notifications', 'fas fa-bell', Notification::class)
+            ->setController(NotificationCrudController::class);
     }
 }
