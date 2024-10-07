48b4cfc3db8b261bc4f808d6c820509e60ed14f4
e-joll <eleonore.jollivet@gmail.com>
2024-10-07 20:01:42 +0200

    Add createIndexQueryBuilder to filter appointments for logged-in agent or customer

--- src/Controller/Admin/Director/AppointmentCrudController.php
+++ src/Controller/Admin/Director/AppointmentCrudController.php
@@ -4,17 +4,19 @@ namespace App\Controller\Admin\Director;
 
 use App\Entity\Appointment;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
-use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\CrudDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 use Symfony\Bundle\SecurityBundle\Security;
 
@@ -61,6 +63,7 @@ class AppointmentCrudController extends AbstractCrudController
                     'Passé' => 'danger',
                 ])
                 ->hideOnForm(),
+            AssociationField::new('buyer', 'Acheteur')
         ];
     }
 
@@ -85,5 +88,18 @@ class AppointmentCrudController extends AbstractCrudController
         $entityInstance->setBuyer($user);
 
         parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $queryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+        if ($this->isGranted('ROLE_CLIENT') or $this->isGranted('ROLE_CUSTOMER')) {
+            $user = $this->getUser();
+            $queryBuilder->andWhere('entity.buyer = :user')
+                ->setParameter('user', $user);
+        }
+
+        return $queryBuilder;
     }
 }
