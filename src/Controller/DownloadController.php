040c81b06b8d6093d0b7fea4e604f0b5e3d33755
e-joll <eleonore.jollivet@gmail.com>
2024-11-09 17:24:23 +0100

    Add DownloadController

---
+++ src/Controller/DownloadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DownloadController extends AbstractController
+{
+    #[Route('/download', name: 'app_download')]
+    public function index(): Response
+    {
+        return $this->render('download/index.html.twig', [
+            'controller_name' => 'DownloadController',
+        ]);
+    }
+}
